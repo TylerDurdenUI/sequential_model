4c45139aae3e5ca4f6ea289b62a94dcd8d9b6d91
Xudong <xzhangnov@gmail.com>
2023-04-24 16:07:34 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
-classifiers=[
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-],
+We are excited to announce a new initiative to strengthen collaboration between our data science and engineering teams. Starting next week, we will be holding weekly half-hour meetings, aimed at facilitating open dialogue, addressing data-related issues, and sharing knowledge on various topics.
+
+Meeting Details:
+
+Goal: Improve collaboration between data scientists and engineers
+Format: 30-minute Q&A discussion
+Frequency: Weekly
+During each meeting, all data scientists and engineers are encouraged to participate in discussions, ask questions, and share insights. Topics can range from ad-hoc data issues and general technologies to platforms, access, and more. The aim is to create an environment that fosters open communication and enhances our collective understanding.
+
+If no questions are raised during the group discussion, we will move to separate meeting rooms for more focused, one-on-one data conversations. If no questions arise in this setting either, feel free to drop from the meeting.
+
+We believe these weekly meetings will help bridge any gaps between our teams, leading to a more efficient and cohesive working relationship. A calendar invite with the specific time and virtual meeting link will be sent out shortly. We look forward to your active participation in this new initiative.
+
+Let's work together to make these meetings engaging, informative, and productive for everyone involved.
+
+Best regards,

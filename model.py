ae713ce815952ed4409eebf300cc05e5621b0b77
Xudong <xzhangnov@gmail.com>
2023-05-10 12:04:02 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,16 +1,28 @@
-We are excited to announce a new initiative to strengthen collaboration between our data science and engineering teams. Starting next week, we will be holding weekly half-hour meetings, aimed at facilitating open dialogue, addressing data-related issues, and sharing knowledge on various topics.
+# Add a page
+pdf.add_page()
 
-Meeting Details:
+# Add summary statistics
+pdf.set_font("Arial", size=12)
+pdf.cell(0, 10, "Summary Statistics:", ln=True)
+pdf.ln(5)
+for row in summary_stats.itertuples():
+    row_data = [row.Index] + [f"{x:.2f}" for x in row[1:]]
+    for i in range(len(row_data)):
+        pdf.cell(0, 10, row_data[i], border=1, ln=(i==len(row_data)-1))
 
-Goal: Improve collaboration between data scientists and engineers
-Format: 30-minute Q&A discussion
-Frequency: Weekly
-During each meeting, all data scientists and engineers are encouraged to participate in discussions, ask questions, and share insights. Topics can range from ad-hoc data issues and general technologies to platforms, access, and more. The aim is to create an environment that fosters open communication and enhances our collective understanding.
+pdf.ln(10)
 
-If no questions are raised during the group discussion, we will move to separate meeting rooms for more focused, one-on-one data conversations. If no questions arise in this setting either, feel free to drop from the meeting.
+# Add head view
+pdf.cell(0, 10, "Head View:", ln=True)
+pdf.ln(5)
+for row in head_view.itertuples():
+    row_data = [str(x) for x in row[1:]]
+    for i in range(len(row_data)):
+        pdf.cell(0, 10, row_data[i], border=1, ln=(i==len(row_data)-1))
 
-We believe these weekly meetings will help bridge any gaps between our teams, leading to a more efficient and cohesive working relationship. A calendar invite with the specific time and virtual meeting link will be sent out shortly. We look forward to your active participation in this new initiative.
+pdf.ln(10)
 
-Let's work together to make these meetings engaging, informative, and productive for everyone involved.
-
-Best regards,
+# Add histogram plot
+pdf.cell(0, 10, "Histogram Plot:", ln=True)
+pdf.ln(5)
+pdf.image("histogram.png", w=pdf.get_page_width() - 40, h=100)
